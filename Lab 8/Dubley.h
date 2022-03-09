f45cb17e81760c664e9ca54d2fe44e21a7788c9f
Andrew McCrary <ajmccrary03@gmail.com>
2022-03-09 14:51:43 -0500

    Dubley Template

--- Lab 8/Dubley.h
+++ Lab 8/Dubley.h
@@ -1,11 +1,21 @@
 #pragma once
+#include "Part.h"
 
 template <typename T>
 class Dubley
 {
 public:
 	Dubley();
-
+	~Dubley();
+	void AddItem(T var);
+	T* GetItem(T var);
+	bool IsInList(T var);
+	bool IsEmpty();
+	int Size();
+	T* SeeNext();
+	T* SeePrev();
+	T* SeeAt();
+	void Reset();
 private:
 
 };
